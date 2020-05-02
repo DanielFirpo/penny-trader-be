1b4ef62c01c704efe41cea869cc06762d377e072
Turtled <trtld2@gmail.com>
2020-05-02 03:12:32 -0700

    Work on add product, edit product, product list, product search, & delete product

--- mysqlConfig.js
+++ mysqlConfig.js
@@ -6,22 +6,35 @@ mysqlObj = {
     password: process.env.MYSQL_PASS,
     database: process.env.MYSQL_DATABASE,
     multipleStatements: true,
+    waitTimeout: 2
 }
 
 var mysqlConnection = mysql.createConnection(mysqlObj)
 
 console.log(mysqlObj)
 
-mysqlConnection.connect((err) => {
-    if (!err) {
-        console.log("Connected to MySQL successfully");
-    } else {
-        console.log("MySQL connection failed: " + err.message);
-    }
-})
+function handleDisconnect() {
+    connection = mysql.createConnection(mysqlObj);
 
-mysqlConnection.on('error', function(err) {
-    console.log("mysql error: ", err);
-});
+    connection.connect(function (err) {
+        if (err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+        else {
+            console.log("Connected to MySQL successfully");
+        }
+    });
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            console.log("mysql error: ", err);
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = mysqlConnection;
--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,15 +1,20 @@
 var express = require("express")
 var router = express.Router()
+const path = require("path");
+const multer = require("multer");
 
 var adminRestricted = require("../../middleware/adminRestricted")
 
 var db = require('../../mysqlConfig');
 
+const amountPerPage = 100;
+
 router.get('/products', adminRestricted, (req, res) => {
-    if(!req.body.pageNumber) {
-        req.body.pageNumber = 0
+    console.log("page number: " + req.query.pageNumber)
+    if(!req.query.pageNumber) {
+        req.query.pageNumber = 0
     }
-    db.query(`SELECT * FROM coins WHERE id BETWEEN ${req.body.pageNumber * 100} AND ${req.body.pageNumber * 100 + 100}`, (err, rows) => {
+    db.query(`SELECT * FROM coins WHERE id LIMIT ${req.query.pageNumber * amountPerPage}, ${req.query.pageNumber * amountPerPage + amountPerPage}`, (err, rows) => {
         if(err) {
             console.log(JSON.stringify(err))
             return res.status(400).send({ message: "Something went wrong. Please try again.", errors: err })
@@ -19,19 +24,46 @@ router.get('/products', adminRestricted, (req, res) => {
 })
 
 router.get('/search', adminRestricted, (req, res) => {
-    
+    if(!req.query.pageNumber) {
+        req.query.pageNumber = 0
+    }
     db.query(
-    `SELECT * FROM coins WHERE name LIKE '%${req.body.searchTerm}%' OR image_name LIKE '%${req.body.searchTerm}%' 
-    OR year LIKE '%${req.body.searchTerm}%' OR description LIKE '%${req.body.searchTerm}%'`, (err, rows) => {
+    `SELECT * FROM coins WHERE name LIKE '%${req.query.searchTerm}%' OR image_name LIKE '%${req.query.searchTerm}%' 
+    OR year LIKE '%${req.query.searchTerm}%' OR description LIKE '%${req.query.searchTerm}%' 
+    LIMIT ${req.query.pageNumber * amountPerPage}, ${req.query.pageNumber * amountPerPage + amountPerPage}`, (err, rows) => {
         if(err) {
             console.log(JSON.stringify(err))
             return res.status(400).send({ message: "Something went wrong. Please try again.", errors: err })
         }
+        console.log("Search: " + rows)
         res.status(200).send({data: rows});
     })
 })
 
-router.post('/add', adminRestricted, (req, res) => {
+const imageFilter = function (req, file, cb) {
+    // accept image only
+    if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
+        console.log("NOT IMAGE")
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    console.log("IMAGE")
+    cb(null, true);
+};
+
+const storage = multer.diskStorage({
+    destination: "./public/images/products",
+    filename: function(req, file, cb){
+       cb(null,"coin-" + Date.now() + "-" + path.basename(file.originalname, path.extname(file.originalname)) + path.extname(file.originalname));
+    }
+ });
+ 
+ const upload = multer({
+    storage: storage,
+    limits:{fileSize: 100000000},
+    fileFilter: imageFilter
+ }).single("coinImage");
+
+router.post('/add', adminRestricted, upload, (req, res) => {
     //req.body.data format:
     // {
     //     name: "name",
@@ -40,46 +72,54 @@ router.post('/add', adminRestricted, (req, res) => {
     //     price: 10000,
     //     description: "desc"
     // }
-    console.log(req)
-    if(!req.body.data){
+
+    if(!req.body){
         return res.status(400).send({ message: "Something went wrong. Make sure you filled out every form field.", errors: ["no data sent"] })
     }
-    if(!req.body.data.name || !req.body.data.image_name || !req.body.data.year || !req.body.data.price || !req.body.data.description){
+    if(!req.body.name || !req.body.year || !req.body.price){
         return res.status(400).send({ message: "Something went wrong. Make sure you filled out every form field.", errors: ["data sent but missing an item"] })
     }
-    db.query(`INSERT INTO coins (name, image_name, year, price, description) VALUES ('${req.body.data.name}', '${req.body.data.image_name}', ${req.body.data.year}, ${req.body.data.price}, '${req.body.data.description}')`, (err, rows) => {
+    let filename = "no-image.png"
+    if(req.file) {
+        filename = req.file.filename; 
+    }
+    if(!req.body.description){
+        req.body.description = ""
+    }
+    db.query(`INSERT INTO coins (name, image_name, year, price, description) VALUES ('${req.body.name}', '${filename}', ${req.body.year}, ${req.body.price * 100}, '${req.body.description}')`, (err, rows) => {
         if(err) {
+            console.log("SQL Error")
             return res.status(400).send({ message: "Something went wrong. Please try again.", errors: err })
         }
         return res.status(200).send({message: "Added product successfully."});
     })
 })
 
-router.post('/edit', adminRestricted, (req, res) => {
+router.post('/edit', adminRestricted, upload, (req, res) => {
     if(!req.body.id) {
         return res.status(400).send({ message: "Something went wrong. Please try again.", errors: ["no id to edit sent"] })
     }
-    if(!req.body.data){
+    if(!req.body){
         return res.status(400).send({ message: "Something went wrong. Make sure you filled out every form field.", errors: ["no data sent"] })
     }
     // if(!req.data.name || !req.data.image_name || !req.data.year || !req.data.price || !req.data.description){
     //     return res.status(400).send({ message: "Something went wrong. Make sure you filled out every form field.", errors: ["data sent but missing an item"] })
-    // }
+    // }    let filename = "no-image.png"
     let valuesToUpdate = ""
-    if (req.body.data.name) { 
-        valuesToUpdate += `name = '${req.body.data.name}', `;
+    if (req.body.name) { 
+        valuesToUpdate += `name = '${req.body.name}', `;
     }
-    if (req.body.data.image_name) {
-        valuesToUpdate += `image_name = '${req.body.data.image_name}', `
+    if (req.file && req.file.filename) {
+        valuesToUpdate += `image_name = '${req.file.filename}', `
     }
-    if (req.body.data.year) { 
-        valuesToUpdate += `year = '${req.body.data.year}', `
+    if (req.body.year) { 
+        valuesToUpdate += `year = '${req.body.year}', `
     }
-    if (req.body.data.price) {
-        valuesToUpdate += `price = '${req.body.data.price}', `
+    if (req.body.price) {
+        valuesToUpdate += `price = '${req.body.price}', `
     }
-    if (req.body.data.description) {
-        valuesToUpdate += `description = '${req.body.data.description}'`
+    if (req.body.description) {
+        valuesToUpdate += `description = '${req.body.description}'`
     }
 
     db.query(`UPDATE coins SET ${valuesToUpdate} WHERE id = ${req.body.id}`, (err, rows) => {
@@ -87,6 +127,20 @@ router.post('/edit', adminRestricted, (req, res) => {
             return res.status(400).send({ message: "Something went wrong. Please try again.", errors: err })
         }
         return res.status(200).send({message: "Edited product successfully."});
+    })
+})
+
+router.delete('/delete', adminRestricted, (req, res) => {
+    if(!req.query.id) {
+        return res.status(400).send({ message: "Something went wrong. Please try again.", error: "no id provided"})
+    }
+    id = req.query.id
+    db.query(`DELETE FROM coins WHERE id = ${id}`, (err, rows) => {
+        if(err) {
+            console.log("SQL error", err)
+            return res.status(400).send({ message: "Something went wrong. Please try again.", errors: err })
+        }
+        return res.status(200).send({message: "Deleted product successfully."});
     })
 })
 
