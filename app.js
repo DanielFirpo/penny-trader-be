12fbd0023eb51aaefbdfcab8fbba0acce72641b4
Turtled <trtld2@gmail.com>
2020-08-29 08:55:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser())
+app.use(bodyParser.json({limit:'20mb'})); 
+app.use(bodyParser.urlencoded({extended:true, limit:'20mb'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // // catch 404 and forward to error handler
